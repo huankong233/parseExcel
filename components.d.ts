a6f2871921684d9e968b914fdd52a84e60d530c5
huan_kong <2564076459@qq.com>
2024-04-01 22:52:25 +0800

    fix build problem

--- components.d.ts
+++ components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     ElAlert: typeof import('element-plus/es')['ElAlert']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
-    ElColorPicker: typeof import('element-plus/es')['ElColorPicker']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
