86b114bb56dbd3b87809aebfebc7112eb001f24b
huan_kong <huank0ng@foxmail.com>
2024-05-24 15:48:40 +0800

    [*] 优化打包后大小

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/main.scss'
+// import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/theme-chalk/el-message.css'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
 import App from './App.vue'
 
-// import Vconsole from 'vconsole'
-// let vConsole = new Vconsole()
-
-const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
-app.use(ElementPlus)
-// app.use(vConsole)
+app.use(createPinia())
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -5,32 +5,42 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 // import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: 'terser',
-    // terserOptions: {
-    //   compress: {
-    //     // 生产环境时移除console
-    //     drop_console: true,
-    //     drop_debugger: true
-    //   }
-    // }
+    terserOptions: {
+      compress: {
+        // 生产环境时移除console
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    //   关闭文件计算
+    reportCompressedSize: false,
+    //   关闭生成map文件 可以达到缩小打包体积
+    sourcemap: false, // 这个生产环境一定要关闭，不然打包的产物会很大
     rollupOptions: {
       output: {
         chunkFileNames: 'assets/js/[name]-[hash].js', // 产生的 chunk 自定义命名
         entryFileNames: 'assets/js/[name]-[hash].js', // 指定 chunks 的入口文件匹配模式
         assetFileNames: 'assets/[ext]/[name]-[hash].[ext]', // 自定义构建结果中的静态资源名称，资源文件像 字体，图片等
-        manualChunks(id) {
-          if (id.includes('lodash-es')) return 'lodash'
-          if (id.includes('xlsx')) return 'xlsx'
-          if (id.includes('jszip')) return 'jszip'
-          if (id.includes('file-saver')) return 'fileSaver'
-          if (id.includes('element-plus/es')) return 'elementPlusEs'
-          if (id.includes('element-plus/theme')) return 'elementPlusTheme'
-          if (id.includes('node_modules')) return 'vendor'
+        // manualChunks(id) {
+        //   if (id.includes('lodash-es')) return 'lodash'
+        //   if (id.includes('xlsx')) return 'xlsx'
+        //   if (id.includes('jszip')) return 'jszip'
+        //   if (id.includes('file-saver')) return 'fileSaver'
+        //   if (id.includes('element-plus/es')) return 'elementPlusEs'
+        //   if (id.includes('element-plus/theme')) return 'elementPlusTheme'
+        //   if (id.includes('node_modules')) return 'vendor'
+        // }
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
         }
       }
     }
@@ -42,6 +52,12 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
+    }),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      algorithm: 'brotliCompress',
+      ext: '.gz'
     })
     // visualizer({
     //   gzipSize: true,
